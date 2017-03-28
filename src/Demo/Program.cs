ebe4a4a19e55697abf4acde512a6a867818b10c0
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2017-03-29 01:49:13 +0200

    Revert testcode

--- src/Demo/Program.cs
+++ src/Demo/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.IO;
-using TestableFileSystem.Interfaces;
+﻿using TestableFileSystem.Interfaces;
 using TestableFileSystem.Wrappers;
 
 namespace TestableFileSystem.Demo
@@ -9,11 +8,6 @@ namespace TestableFileSystem.Demo
         private static void Main()
         {
             IFileSystem fileSystem = FileSystemWrapper.Default;
-
-            DirectoryInfo info = new DirectoryInfo(@"e:\FileSystemTests\subdir\other\deeper");
-            info.Attributes = FileAttributes.ReadOnly;
-
-            fileSystem.Directory.Delete(@"e:\FileSystemTests\subdir", true);
         }
     }
 }
