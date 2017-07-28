5666e28d88725c138c6d55f42e554de5c4083c0b
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2017-07-28 21:17:12 +0200

    Inlined DirectoryTreeBuilder

--- src/Fakes/Builders/FakeFileSystemBuilder.cs
+++ src/Fakes/Builders/FakeFileSystemBuilder.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text;
 using JetBrains.Annotations;
 using TestableFileSystem.Interfaces;
@@ -12,16 +13,15 @@ namespace TestableFileSystem.Fakes.Builders
         private bool includeDriveC = true;
 
         [NotNull]
-        private readonly DirectoryTreeBuilder builder = new DirectoryTreeBuilder();
+        private readonly DirectoryEntry root = DirectoryEntry.CreateRoot();
 
         public FakeFileSystem Build()
         {
             if (includeDriveC)
             {
-                builder.IncludingDirectory("C:");
+                IncludingDirectory("C:");
             }
 
-            DirectoryEntry root = builder.Build();
             return new FakeFileSystem(root);
         }
 
@@ -33,11 +33,29 @@ namespace TestableFileSystem.Fakes.Builders
         }
 
         [NotNull]
+        public FakeFileSystemBuilder IncludingDirectory([NotNull] string path, [CanBeNull] FileAttributes? attributes = null)
+        {
+            Guard.NotNull(path, nameof(path));
+
+            var absolutePath = new AbsolutePath(path);
+            var navigator = new PathNavigator(absolutePath);
+
+            DirectoryEntry directory = root.CreateDirectories(navigator);
+
+            if (attributes != null)
+            {
+                directory.Attributes = attributes.Value;
+            }
+
+            return this;
+        }
+
+        [NotNull]
         public FakeFileSystemBuilder IncludingEmptyFile([NotNull] string path, [CanBeNull] FileAttributes? attributes = null)
         {
             Guard.NotNull(path, nameof(path));
 
-            builder.IncludingEmptyFile(path, attributes);
+            IncludeFile(path, entry => { }, attributes);
             return this;
         }
 
@@ -48,8 +66,18 @@ namespace TestableFileSystem.Fakes.Builders
             Guard.NotNull(path, nameof(path));
             Guard.NotNullNorEmpty(contents, nameof(contents));
 
-            builder.IncludingTextFile(path, contents, encoding, attributes);
+            IncludeFile(path, entry => WriteStringToFile(entry, contents, encoding), attributes);
             return this;
+        }
+
+        private static void WriteStringToFile([NotNull] IFileStream fileStream, [NotNull] string text,
+            [CanBeNull] Encoding encoding)
+        {
+            Stream stream = fileStream.AsStream();
+            using (StreamWriter writer = encoding == null ? new StreamWriter(stream) : new StreamWriter(stream, encoding))
+            {
+                writer.Write(text);
+            }
         }
 
         [NotNull]
@@ -59,17 +87,55 @@ namespace TestableFileSystem.Fakes.Builders
             Guard.NotNull(path, nameof(path));
             Guard.NotNullNorEmpty(contents, nameof(contents));
 
-            builder.IncludingBinaryFile(path, contents, attributes);
+            IncludeFile(path, entry => WriteBytesToFile(entry, contents), attributes);
             return this;
         }
 
-        [NotNull]
-        public FakeFileSystemBuilder IncludingDirectory([NotNull] string path, [CanBeNull] FileAttributes? attributes = null)
+        private static void WriteBytesToFile([NotNull] IFileStream stream, [NotNull] byte[] buffer)
         {
-            Guard.NotNull(path, nameof(path));
+            stream.Write(buffer, 0, buffer.Length);
+        }
 
-            builder.IncludingDirectory(path, attributes);
-            return this;
+        private void IncludeFile([NotNull] string path, [NotNull] Action<IFileStream> writeContentsToStream,
+            [CanBeNull] FileAttributes? attributes)
+        {
+            var absolutePath = new AbsolutePath(path);
+            var navigator = new PathNavigator(absolutePath);
+
+            AssertDoesNotExistAsDirectory(absolutePath);
+            RemoveExistingFile(absolutePath);
+
+            FileEntry file = root.GetOrCreateFile(navigator, true);
+
+            using (IFileStream stream = file.Open(FileMode.Open, FileAccess.Write))
+            {
+                writeContentsToStream(stream);
+            }
+
+            if (attributes != null)
+            {
+                file.Attributes = attributes.Value;
+            }
+        }
+
+        private void AssertDoesNotExistAsDirectory([NotNull] AbsolutePath path)
+        {
+            var navigator = new PathNavigator(path);
+            DirectoryEntry directory = root.TryGetExistingDirectory(navigator);
+            if (directory != null)
+            {
+                throw ErrorFactory.CannotCreateBecauseFileOrDirectoryAlreadyExists(path.GetText());
+            }
+        }
+
+        private void RemoveExistingFile([NotNull] AbsolutePath absolutePath)
+        {
+            var navigator = new PathNavigator(absolutePath);
+            FileEntry file = root.TryGetExistingFile(navigator);
+            if (file != null)
+            {
+                root.DeleteFile(navigator);
+            }
         }
     }
 }
