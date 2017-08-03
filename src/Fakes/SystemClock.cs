828144a24871a7a405f20026dd9b913929d08a42
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2017-08-04 01:35:12 +0200

    Removed unused member

--- src/Fakes/SystemClock.cs
+++ src/Fakes/SystemClock.cs
@@ -5,10 +5,6 @@ namespace TestableFileSystem.Fakes
 {
     public sealed class SystemClock
     {
-        // TODO: Remove this member.
-        [NotNull]
-        public Func<DateTime> Now = () => DateTime.Now;
-
         [NotNull]
         public Func<DateTime> UtcNow = () => DateTime.UtcNow;
     }
