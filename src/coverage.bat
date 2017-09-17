108f434d9e8dcced37bdf937024d82448a92ea2b
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2017-09-18 00:53:01 +0200

    Fixed code coverage to include analyzer tests

--- src/coverage.bat
+++ src/coverage.bat
@@ -5,8 +5,8 @@ md coverage
 set opencover="%USERPROFILE%\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe"
 set reportgenerator="%USERPROFILE%\.nuget\packages\ReportGenerator\2.5.10\tools\ReportGenerator.exe"
 set testrunner="%USERPROFILE%\.nuget\packages\xunit.runner.console\2.2.0\tools\xunit.console.x86.exe"
-set target=".\Fakes.Tests\bin\Debug\net452\TestableFileSystem.Fakes.Tests.dll -noshadow"
+set targets=".\Fakes.Tests\bin\Debug\net452\TestableFileSystem.Fakes.Tests.dll .\Analyzer.Tests\bin\Debug\net452\TestableFileSystem.Analyzer.Tests.dll -noshadow"
 set coveragefile=".\coverage\TestableFileSystem.xml"
 
-%opencover% -register:user -target:%testrunner% -targetargs:%target% -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -output:%coveragefile%
+%opencover% -register:user -target:%testrunner% -targetargs:%targets% -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -output:%coveragefile%
 %reportgenerator% -targetdir:.\coverage -reports:%coveragefile%
