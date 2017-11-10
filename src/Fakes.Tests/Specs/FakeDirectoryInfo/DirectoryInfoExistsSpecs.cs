0f3a59d7381730d64d196cf816f6a404985ff791
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2017-11-10 17:31:49 +0100

    Added specs for DirectoryInfo.Delete

--- src/Fakes.Tests/Specs/FakeDirectoryInfo/DirectoryInfoExistsSpecs.cs
+++ src/Fakes.Tests/Specs/FakeDirectoryInfo/DirectoryInfoExistsSpecs.cs
@@ -1,7 +1,105 @@
-﻿namespace TestableFileSystem.Fakes.Tests.Specs.FakeDirectoryInfo
+﻿using FluentAssertions;
+using TestableFileSystem.Fakes.Builders;
+using TestableFileSystem.Interfaces;
+using Xunit;
+
+namespace TestableFileSystem.Fakes.Tests.Specs.FakeDirectoryInfo
 {
     public sealed class DirectoryInfoExistsSpecs
     {
-        // TODO: Add specs
+        [Fact]
+        private void When_getting_directory_existence_it_must_lazy_load()
+        {
+            // Arrange
+            const string path = @"c:\some\folder";
+
+            IFileSystem fileSystem = new FakeFileSystemBuilder()
+                .IncludingDirectory(@"c:\some")
+                .Build();
+
+            IDirectoryInfo dirInfo = fileSystem.ConstructDirectoryInfo(path);
+
+            fileSystem.Directory.CreateDirectory(path);
+
+            // Act
+            bool found = dirInfo.Exists;
+
+            // Assert
+            found.Should().BeTrue();
+        }
+
+        [Fact]
+        private void When_getting_directory_existence_it_must_cache()
+        {
+            // Arrange
+            const string path = @"c:\some\folder";
+
+            IFileSystem fileSystem = new FakeFileSystemBuilder()
+                .IncludingDirectory(@"c:\some")
+                .Build();
+
+            IDirectoryInfo dirInfo = fileSystem.ConstructDirectoryInfo(path);
+
+            bool beforeFound = dirInfo.Exists;
+
+            fileSystem.Directory.CreateDirectory(path);
+
+            // Act
+            bool afterFound = dirInfo.Exists;
+
+            // Assert
+            beforeFound.Should().BeFalse();
+            afterFound.Should().BeFalse();
+        }
+
+        [Fact]
+        private void When_getting_directory_existence_after_external_change_it_must_update_cache_on_refresh()
+        {
+            // Arrange
+            const string path = @"c:\some\folder";
+
+            IFileSystem fileSystem = new FakeFileSystemBuilder()
+                .IncludingDirectory(@"c:\some")
+                .Build();
+
+            IDirectoryInfo dirInfo = fileSystem.ConstructDirectoryInfo(path);
+
+            bool beforeFound = dirInfo.Exists;
+
+            fileSystem.Directory.CreateDirectory(path);
+
+            // Act
+            dirInfo.Refresh();
+
+            // Assert
+            bool afterFound = dirInfo.Exists;
+
+            beforeFound.Should().BeFalse();
+            afterFound.Should().BeTrue();
+        }
+
+        [Fact]
+        private void When_changing_directory_existence_it_must_not_refresh_automatically()
+        {
+            // Arrange
+            const string path = @"c:\some\folder";
+
+            IFileSystem fileSystem = new FakeFileSystemBuilder()
+                .IncludingDirectory(path)
+                .Build();
+
+            IDirectoryInfo dirInfo = fileSystem.ConstructDirectoryInfo(path);
+
+            bool beforeFound = dirInfo.Exists;
+
+            // Act
+            dirInfo.Delete();
+
+            // Assert
+            bool afterFound = dirInfo.Exists;
+
+            beforeFound.Should().BeTrue();
+            afterFound.Should().BeTrue();
+        }
     }
 }
