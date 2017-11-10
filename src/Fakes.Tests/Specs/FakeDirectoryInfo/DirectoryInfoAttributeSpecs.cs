9a065e577bf8f2e8ea2abc54164c2c39244a3e09
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2017-11-10 17:26:13 +0100

    Added specs for DirectoryInfo.Attributes

--- src/Fakes.Tests/Specs/FakeDirectoryInfo/DirectoryInfoAttributeSpecs.cs
+++ src/Fakes.Tests/Specs/FakeDirectoryInfo/DirectoryInfoAttributeSpecs.cs
@@ -1,7 +1,106 @@
-﻿namespace TestableFileSystem.Fakes.Tests.Specs.FakeDirectoryInfo
+﻿using System.IO;
+using FluentAssertions;
+using TestableFileSystem.Fakes.Builders;
+using TestableFileSystem.Interfaces;
+using Xunit;
+
+namespace TestableFileSystem.Fakes.Tests.Specs.FakeDirectoryInfo
 {
     public sealed class DirectoryInfoAttributeSpecs
     {
-        // TODO: Add specs
+        [Fact]
+        private void When_getting_directory_attributes_it_must_lazy_load()
+        {
+            // Arrange
+            const string path = @"c:\some\folder";
+
+            IFileSystem fileSystem = new FakeFileSystemBuilder()
+                .IncludingDirectory(path, FileAttributes.Hidden)
+                .Build();
+
+            IDirectoryInfo dirInfo = fileSystem.ConstructDirectoryInfo(path);
+
+            fileSystem.File.SetAttributes(path, FileAttributes.ReadOnly);
+
+            // Act
+            FileAttributes attributes = dirInfo.Attributes;
+
+            // Assert
+            attributes.Should().Be(FileAttributes.Directory | FileAttributes.ReadOnly);
+        }
+
+        [Fact]
+        private void When_getting_directory_attributes_it_must_cache()
+        {
+            // Arrange
+            const string path = @"c:\some\folder";
+
+            IFileSystem fileSystem = new FakeFileSystemBuilder()
+                .IncludingDirectory(path, FileAttributes.Hidden)
+                .Build();
+
+            IDirectoryInfo dirInfo = fileSystem.ConstructDirectoryInfo(path);
+
+            FileAttributes beforeAttributes = dirInfo.Attributes;
+
+            fileSystem.File.SetAttributes(path, FileAttributes.ReadOnly);
+
+            // Act
+            FileAttributes afterAttributes = dirInfo.Attributes;
+
+            // Assert
+            beforeAttributes.Should().Be(FileAttributes.Directory | FileAttributes.Hidden);
+            afterAttributes.Should().Be(FileAttributes.Directory | FileAttributes.Hidden);
+        }
+
+        [Fact]
+        private void When_getting_directory_attributes_after_external_change_it_must_update_cache_on_refresh()
+        {
+            // Arrange
+            const string path = @"c:\some\folder";
+
+            IFileSystem fileSystem = new FakeFileSystemBuilder()
+                .IncludingDirectory(path, FileAttributes.Hidden)
+                .Build();
+
+            IDirectoryInfo dirInfo = fileSystem.ConstructDirectoryInfo(path);
+
+            FileAttributes beforeAttributes = dirInfo.Attributes;
+
+            fileSystem.File.SetAttributes(path, FileAttributes.ReadOnly);
+
+            // Act
+            dirInfo.Refresh();
+
+            // Assert
+            FileAttributes afterAttributes = dirInfo.Attributes;
+
+            beforeAttributes.Should().Be(FileAttributes.Directory | FileAttributes.Hidden);
+            afterAttributes.Should().Be(FileAttributes.Directory | FileAttributes.ReadOnly);
+        }
+
+        [Fact]
+        private void When_changing_directory_attributes_it_must_refresh_automatically()
+        {
+            // Arrange
+            const string path = @"c:\some\folder";
+
+            IFileSystem fileSystem = new FakeFileSystemBuilder()
+                .IncludingDirectory(path, FileAttributes.Hidden)
+                .Build();
+
+            IDirectoryInfo dirInfo = fileSystem.ConstructDirectoryInfo(path);
+
+            FileAttributes beforeAttributes = dirInfo.Attributes;
+
+            // Act
+            dirInfo.Attributes = FileAttributes.ReadOnly;
+
+            // Assert
+            FileAttributes afterAttributes = dirInfo.Attributes;
+
+            beforeAttributes.Should().Be(FileAttributes.Directory | FileAttributes.Hidden);
+            afterAttributes.Should().Be(FileAttributes.Directory | FileAttributes.ReadOnly);
+        }
     }
 }
