9985be52a39151f407a434b9dec53943d41d5484
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2017-11-15 09:38:08 +0100

    Added specs for DirectoryInfo.Create

--- src/Fakes.Tests/Specs/FakeDirectoryInfo/DirectoryInfoCreateSpecs.cs
+++ src/Fakes.Tests/Specs/FakeDirectoryInfo/DirectoryInfoCreateSpecs.cs
@@ -1,7 +1,51 @@
-﻿namespace TestableFileSystem.Fakes.Tests.Specs.FakeDirectoryInfo
+﻿using FluentAssertions;
+using TestableFileSystem.Fakes.Builders;
+using TestableFileSystem.Interfaces;
+using Xunit;
+
+namespace TestableFileSystem.Fakes.Tests.Specs.FakeDirectoryInfo
 {
     public sealed class DirectoryInfoCreateSpecs
     {
-        // TODO: Add specs
+        [Fact]
+        private void When_creating_directory_it_must_succeed()
+        {
+            // Arrange
+            const string path = @"d:\some\folder\nested";
+
+            IFileSystem fileSystem = new FakeFileSystemBuilder()
+                .IncludingDirectory(@"d:\")
+                .Build();
+
+            IDirectoryInfo dirInfo = fileSystem.ConstructDirectoryInfo(path);
+
+            // Act
+            dirInfo.Create();
+
+            // Assert
+            fileSystem.Directory.Exists(path).Should().BeTrue();
+        }
+
+        [Fact]
+        private void When_creating_directory_it_must_not_update_properties()
+        {
+            // Arrange
+            const string path = @"d:\some\folder\nested";
+
+            IFileSystem fileSystem = new FakeFileSystemBuilder()
+                .IncludingDirectory(@"d:\")
+                .Build();
+
+            IDirectoryInfo dirInfo = fileSystem.ConstructDirectoryInfo(path);
+
+            bool beforeExists = dirInfo.Exists;
+
+            // Act
+            dirInfo.Create();
+
+            // Assert
+            beforeExists.Should().BeFalse();
+            dirInfo.Exists.Should().BeFalse();
+        }
     }
 }
