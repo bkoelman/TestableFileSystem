63bcd730a309eb1b3f0e41e50fa4365759fa210a
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2017-12-12 22:49:57 +0100

    Package updates

--- coverage.bat
+++ coverage.bat
@@ -4,7 +4,7 @@ md coverage
 
 set configuration=Debug
 set opencover="%USERPROFILE%\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe"
-set reportgenerator="%USERPROFILE%\.nuget\packages\ReportGenerator\3.0.2\tools\ReportGenerator.exe"
+set reportgenerator="%USERPROFILE%\.nuget\packages\ReportGenerator\3.1.0\tools\ReportGenerator.exe"
 set testrunner="%USERPROFILE%\.nuget\packages\xunit.runner.console\2.3.1\tools\net452\xunit.console.x86.exe"
 set targets=".\src\Fakes.Tests\bin\%configuration%\net452\TestableFileSystem.Fakes.Tests.dll .\src\Analyzer.Tests\bin\%configuration%\net452\TestableFileSystem.Analyzer.Tests.dll -noshadow"
 set filter="+[TestableFileSystem*]*  -[TestableFileSystem.*.Tests*]*  -[TestableFileSystem.Wrappers*]*"
