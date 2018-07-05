fba3ad5866aaa23d40ce4c940ee1462328422806
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2018-07-05 14:37:46 +0200

    Added change dumper to demo program

---
+++ src/Demo/FileSystemChangeDumper.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using TestableFileSystem.Interfaces;
+
+namespace TestableFileSystem.Demo
+{
+    /// <summary>
+    /// Displays incoming changes from <see cref="IFileSystemWatcher" /> on the console.
+    /// </summary>
+    internal sealed class FileSystemChangeDumper
+    {
+        private const NotifyFilters NotifyFiltersAll = NotifyFilters.FileName | NotifyFilters.DirectoryName |
+            NotifyFilters.Attributes | NotifyFilters.Size | NotifyFilters.LastWrite | NotifyFilters.LastAccess |
+            NotifyFilters.CreationTime | NotifyFilters.Security;
+
+        private readonly IFileSystem fileSystem;
+
+        public FileSystemChangeDumper(IFileSystem fileSystem)
+        {
+            Guard.NotNull(fileSystem, nameof(fileSystem));
+            this.fileSystem = fileSystem;
+        }
+
+        public void Start(string path)
+        {
+            Guard.NotNull(path, nameof(path));
+
+            using (var setupCompletedSignal = new ConcurrentSignal())
+            {
+                StartMonitorChangesOnDiskLoop(path, setupCompletedSignal);
+            }
+
+            Thread.Sleep(250);
+        }
+
+        private void StartMonitorChangesOnDiskLoop(string path, ConcurrentSignal setupCompletedSignal)
+        {
+            Task.Run(() => MonitorChangesOnDiskLoop(path, setupCompletedSignal));
+        }
+
+        private void MonitorChangesOnDiskLoop(string path, ConcurrentSignal setupCompletedSignal)
+        {
+            IFileSystemWatcher watcher = Setup(path);
+            setupCompletedSignal.SetComplete();
+
+            while (true)
+            {
+                GC.KeepAlive(watcher);
+            }
+        }
+
+        private IFileSystemWatcher Setup(string path)
+        {
+            IFileSystemWatcher watcher = fileSystem.ConstructFileSystemWatcher();
+
+            watcher.Created += (sender, args) => DisplayChange(args.ChangeType, args.Name);
+            watcher.Deleted += (sender, args) => DisplayChange(args.ChangeType, args.Name);
+            watcher.Changed += (sender, args) => DisplayChange(args.ChangeType, args.Name);
+            watcher.Renamed += (sender, args) => DisplayChange(args.ChangeType, args.Name, args.OldName);
+            watcher.Error += (sender, args) => DisplayError(args.GetException());
+
+            watcher.IncludeSubdirectories = true;
+            watcher.Path = path;
+            watcher.NotifyFilter = NotifyFiltersAll;
+            watcher.EnableRaisingEvents = true;
+
+            Console.WriteLine($"Start monitoring changes on '{path}'");
+
+            return watcher;
+        }
+
+        private static void DisplayChange(WatcherChangeTypes changeType, string name, string previousPathInRename = null)
+        {
+            string symbol = GetChangeSymbol(changeType);
+
+            Console.WriteLine(changeType == WatcherChangeTypes.Renamed
+                ? $"{symbol} [{Thread.CurrentThread.ManagedThreadId}] {previousPathInRename} => {name}"
+                : $"{symbol} [{Thread.CurrentThread.ManagedThreadId}] {name}");
+        }
+
+        private static void DisplayError(Exception exception)
+        {
+            Console.WriteLine($"ERROR: [{Thread.CurrentThread.ManagedThreadId}] {exception.Message}");
+        }
+
+        private static string GetChangeSymbol(WatcherChangeTypes changeType)
+        {
+            switch (changeType)
+            {
+                case WatcherChangeTypes.Created:
+                {
+                    return "+";
+                }
+                case WatcherChangeTypes.Deleted:
+                {
+                    return "-";
+                }
+                case WatcherChangeTypes.Changed:
+                case WatcherChangeTypes.Renamed:
+                {
+                    return "*";
+                }
+                default:
+                {
+                    return "?";
+                }
+            }
+        }
+
+        private sealed class ConcurrentSignal : IDisposable
+        {
+            private readonly EventWaitHandle waitHandle = new AutoResetEvent(false);
+
+            public void SetComplete()
+            {
+                waitHandle.Set();
+            }
+
+            public void Dispose()
+            {
+                waitHandle.WaitOne();
+            }
+        }
+    }
+}
--- src/Demo/Program.cs
+++ src/Demo/Program.cs
@@ -8,29 +8,53 @@ namespace TestableFileSystem.Demo
 {
     internal static class Program
     {
-        private const string TargetPath = @"C:\";
+        private static readonly string TargetDirectory = Environment.ExpandEnvironmentVariables(@"%TEMP%\TestableFileSystemDemo");
 
         private static void Main()
         {
-            IFileSystem fileSystem = CreateFakeFileSystem();
-            //IFileSystem fileSystem = CreateWrapperFileSystem();
+            IFileSystem fileSystem = CreateFileSystem();
 
-            Console.WriteLine($"Files found in '{TargetPath}':");
-            foreach (string path in fileSystem.Directory.GetFiles(TargetPath, "*.txt", SearchOption.AllDirectories))
+            fileSystem.Directory.CreateDirectory(TargetDirectory);
+            new FileSystemChangeDumper(fileSystem).Start(TargetDirectory);
+
+            DeleteAndCreateFile(fileSystem);
+            ShowFilesInTargetDirectory(fileSystem);
+
+            Console.WriteLine("Press any key to close.");
+            Console.ReadKey();
+        }
+
+        private static void DeleteAndCreateFile(IFileSystem fileSystem)
+        {
+            string fileName = Path.Combine(TargetDirectory, "newfile.txt");
+
+            fileSystem.File.Delete(fileName);
+            fileSystem.File.WriteAllText(fileName, "TestableFileSystem Example File");
+        }
+
+        private static void ShowFilesInTargetDirectory(IFileSystem fileSystem)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Scanning for text files in '{TargetDirectory}'...");
+            foreach (string path in fileSystem.Directory.GetFiles(TargetDirectory, "*.txt", SearchOption.AllDirectories))
             {
                 Console.WriteLine($"File '{path}' contains:");
 
                 string contents = fileSystem.File.ReadAllText(path);
                 Console.WriteLine(contents);
             }
+        }
 
-            Console.WriteLine("Press any key to close.");
-            Console.ReadKey();
+        private static IFileSystem CreateFileSystem()
+        {
+            //return CreateFakeFileSystem();
+            return CreateWrapperFileSystem();
         }
 
         private static IFileSystem CreateFakeFileSystem()
         {
-            return new FakeFileSystemBuilder().IncludingTextFile(@"c:\subdir\demo.txt", "Hello File System").Build();
+            return new FakeFileSystemBuilder()
+                .IncludingTextFile(Path.Combine(TargetDirectory, @"subdir\demo.txt"), "Hello from Fake File System").Build();
         }
 
         private static IFileSystem CreateWrapperFileSystem()
