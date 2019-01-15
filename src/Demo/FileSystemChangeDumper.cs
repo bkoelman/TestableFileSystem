de40c427cdae2e34a728348aaae11d00b4f01efa
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2019-01-15 18:39:02 +0100

    simplified dumper

--- src/Demo/FileSystemChangeDumper.cs
+++ src/Demo/FileSystemChangeDumper.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Threading;
-using System.Threading.Tasks;
 using TestableFileSystem.Interfaces;
 
 namespace TestableFileSystem.Demo
@@ -9,69 +8,60 @@ namespace TestableFileSystem.Demo
     /// <summary>
     /// Displays incoming changes from <see cref="IFileSystemWatcher" /> on the console.
     /// </summary>
-    internal sealed class FileSystemChangeDumper
+    internal sealed class FileSystemChangeDumper : IDisposable
     {
         private const NotifyFilters NotifyFiltersAll = NotifyFilters.FileName | NotifyFilters.DirectoryName |
             NotifyFilters.Attributes | NotifyFilters.Size | NotifyFilters.LastWrite | NotifyFilters.LastAccess |
             NotifyFilters.CreationTime | NotifyFilters.Security;
 
-        private readonly IFileSystem fileSystem;
+        private readonly IFileSystemWatcher watcher;
 
         public FileSystemChangeDumper(IFileSystem fileSystem)
         {
             Guard.NotNull(fileSystem, nameof(fileSystem));
-            this.fileSystem = fileSystem;
-        }
 
-        public void Start(string path, NotifyFilters notifyFilters = NotifyFiltersAll)
-        {
-            Guard.NotNull(path, nameof(path));
-
-            using (var setupCompletedSignal = new ConcurrentSignal())
-            {
-                StartMonitorChangesOnDiskLoop(path, notifyFilters, setupCompletedSignal);
-            }
-
-            Thread.Sleep(250);
-        }
-
-        private void StartMonitorChangesOnDiskLoop(string path, NotifyFilters notifyFilters,
-            ConcurrentSignal setupCompletedSignal)
-        {
-            Task.Run(() => MonitorChangesOnDiskLoop(path, notifyFilters, setupCompletedSignal));
-        }
-
-        private void MonitorChangesOnDiskLoop(string path, NotifyFilters notifyFilters, ConcurrentSignal setupCompletedSignal)
-        {
-            IFileSystemWatcher watcher = Setup(path, notifyFilters);
-            setupCompletedSignal.SetComplete();
-
-            while (true)
-            {
-                GC.KeepAlive(watcher);
-            }
-
-            // ReSharper disable once FunctionNeverReturns
-        }
-
-        private IFileSystemWatcher Setup(string path, NotifyFilters notifyFilters)
-        {
-            IFileSystemWatcher watcher = fileSystem.ConstructFileSystemWatcher();
-
+            watcher = fileSystem.ConstructFileSystemWatcher();
             watcher.Created += (sender, args) => DisplayChange(args.ChangeType, args.Name);
             watcher.Deleted += (sender, args) => DisplayChange(args.ChangeType, args.Name);
             watcher.Changed += (sender, args) => DisplayChange(args.ChangeType, args.Name);
             watcher.Renamed += (sender, args) => DisplayChange(args.ChangeType, args.Name, args.OldName);
             watcher.Error += (sender, args) => DisplayError(args.GetException());
+        }
 
-            watcher.IncludeSubdirectories = true;
-            watcher.Path = path;
-            watcher.NotifyFilter = notifyFilters;
+        public void Start(string path, NotifyFilters filters = NotifyFiltersAll)
+        {
+            Guard.NotNull(path, nameof(path));
+
+            watcher.EnableRaisingEvents = false;
+            SetupWatcher(path, filters);
             watcher.EnableRaisingEvents = true;
 
-            Console.WriteLine($"Start monitoring changes on '{path}'");
+            Thread.Sleep(250);
+        }
 
-            return watcher;
+        private void SetupWatcher(string path, NotifyFilters filters)
+        {
+            Console.WriteLine($"Started watching for changes on '{path}'.");
+
+            if (filters != NotifyFiltersAll)
+            {
+                Console.WriteLine($"Filters: {filters}");
+            }
+
+            watcher.Path = path;
+            watcher.IncludeSubdirectories = true;
+            watcher.NotifyFilter = filters;
+        }
+
+        public void Stop()
+        {
+            watcher.EnableRaisingEvents = false;
+            Console.WriteLine("Stopped watching for changes.");
+        }
+
+        public void Dispose()
+        {
+            Stop();
         }
 
         private static void DisplayChange(WatcherChangeTypes changeType, string name, string previousPathInRename = null)
@@ -79,13 +69,13 @@ namespace TestableFileSystem.Demo
             string symbol = GetChangeSymbol(changeType);
 
             Console.WriteLine(changeType == WatcherChangeTypes.Renamed
-                ? $"{symbol} [{Thread.CurrentThread.ManagedThreadId}] {previousPathInRename} => {name}"
-                : $"{symbol} [{Thread.CurrentThread.ManagedThreadId}] {name}");
+                ? $"{symbol} {previousPathInRename} => {name}"
+                : $"{symbol} {name}");
         }
 
         private static void DisplayError(Exception exception)
         {
-            Console.WriteLine($"ERROR: [{Thread.CurrentThread.ManagedThreadId}] {exception.Message}");
+            Console.WriteLine($"! {exception.Message}");
         }
 
         private static string GetChangeSymbol(WatcherChangeTypes changeType)
@@ -93,40 +83,25 @@ namespace TestableFileSystem.Demo
             switch (changeType)
             {
                 case WatcherChangeTypes.Created:
-                    {
-                        return "+";
-                    }
+                {
+                    return "+";
+                }
                 case WatcherChangeTypes.Deleted:
-                    {
-                        return "-";
-                    }
+                {
+                    return "-";
+                }
                 case WatcherChangeTypes.Changed:
-                    {
-                        return "*";
-                    }
+                {
+                    return "*";
+                }
                 case WatcherChangeTypes.Renamed:
-                    {
-                        return ">";
-                    }
+                {
+                    return ">";
+                }
                 default:
-                    {
-                        return "?";
-                    }
-            }
-        }
-
-        private sealed class ConcurrentSignal : IDisposable
-        {
-            private readonly EventWaitHandle waitHandle = new AutoResetEvent(false);
-
-            public void SetComplete()
-            {
-                waitHandle.Set();
-            }
-
-            public void Dispose()
-            {
-                waitHandle.WaitOne();
+                {
+                    return "?";
+                }
             }
         }
     }
