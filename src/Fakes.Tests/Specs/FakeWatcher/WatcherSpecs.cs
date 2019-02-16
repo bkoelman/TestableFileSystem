4124bc7fe28d6c51b72158b7d20f471a1308c110
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2019-02-16 15:05:21 +0100

    Increase timeout to prevent cibuild failures

--- src/Fakes.Tests/Specs/FakeWatcher/WatcherSpecs.cs
+++ src/Fakes.Tests/Specs/FakeWatcher/WatcherSpecs.cs
@@ -2,7 +2,7 @@
 {
     public abstract class WatcherSpecs
     {
-        protected const int NotifyWaitTimeoutMilliseconds = 500;
+        protected const int NotifyWaitTimeoutMilliseconds = 1000;
         protected const int SleepTimeToEnsureOperationHasArrivedAtWatcherConsumerLoop = 250;
 
         // TODO: Add specs for File.Encrypt/Decrypt, File.Lock/Unlock, File.Replace, Begin+EndRead/Write
