97d4acefef8da8c823ec7a8d92aa8ec277ede0aa
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2019-03-14 12:26:47 +0100

    added watcher specs for async I/O

--- src/Fakes.Tests/Specs/FakeWatcher/NotifyFilter/NotifyFileStreamSpecs.cs
+++ src/Fakes.Tests/Specs/FakeWatcher/NotifyFilter/NotifyFileStreamSpecs.cs
@@ -1,6 +1,9 @@
 ﻿#if !NETCOREAPP1_1
+using System;
 using System.IO;
+using System.Threading.Tasks;
 using FluentAssertions;
+using JetBrains.Annotations;
 using TestableFileSystem.Fakes.Builders;
 using TestableFileSystem.Interfaces;
 using Xunit;
@@ -71,6 +74,134 @@ namespace TestableFileSystem.Fakes.Tests.Specs.FakeWatcher.NotifyFilter
                         // Assert
                         listener.EventsCollected.Should().BeEmpty();
                     }
+                }
+            }
+        }
+
+        [Theory]
+        [WatcherNotifyTestData(@"
+            * Container\file.txt                            @ LastAccess
+        ")]
+        private void When_async_reading_from_stream_it_must_raise_events(NotifyFilters filters, [NotNull] string expectedText)
+        {
+            // Arrange
+            const string directoryToWatch = @"c:\some";
+            const string containerDirectoryName = "Container";
+            const string fileNameToRead = "file.txt";
+
+            string pathToFileToRead = Path.Combine(directoryToWatch, containerDirectoryName, fileNameToRead);
+
+            FakeFileSystem fileSystem = new FakeFileSystemBuilder()
+                .IncludingBinaryFile(pathToFileToRead, BufferFactory.Create(1024))
+                .Build();
+
+            var buffer = new byte[512];
+
+            using (FakeFileSystemWatcher watcher = fileSystem.ConstructFileSystemWatcher(directoryToWatch))
+            {
+                watcher.NotifyFilter = filters;
+                watcher.IncludeSubdirectories = true;
+
+                using (var listener = new FileSystemWatcherEventListener(watcher))
+                {
+                    using (IFileStream stream = fileSystem.File.Open(pathToFileToRead, FileMode.Open, FileAccess.ReadWrite))
+                    {
+                        // Act
+                        Task<int> task = stream.ReadAsync(buffer, 0, buffer.Length);
+                        task.Wait();
+                    }
+
+                    watcher.FinishAndWaitForFlushed(NotifyWaitTimeoutMilliseconds);
+
+                    // Assert
+                    string text = string.Join(Environment.NewLine, listener.GetEventsCollectedAsText());
+                    text.Should().Be(expectedText);
+                }
+            }
+        }
+
+        [Theory]
+        [WatcherNotifyTestData(@"
+            * Container\file.txt                            @ LastAccess    LastWrite
+        ")]
+        private void When_async_writing_to_stream_it_must_raise_events(NotifyFilters filters, [NotNull] string expectedText)
+        {
+            // Arrange
+            const string directoryToWatch = @"c:\some";
+            const string containerDirectoryName = "Container";
+            const string fileNameToWrite = "file.txt";
+
+            string pathToFileToWrite = Path.Combine(directoryToWatch, containerDirectoryName, fileNameToWrite);
+
+            FakeFileSystem fileSystem = new FakeFileSystemBuilder()
+                .IncludingBinaryFile(pathToFileToWrite, BufferFactory.Create(1024))
+                .Build();
+
+            var buffer = new byte[512];
+
+            using (FakeFileSystemWatcher watcher = fileSystem.ConstructFileSystemWatcher(directoryToWatch))
+            {
+                watcher.NotifyFilter = filters;
+                watcher.IncludeSubdirectories = true;
+
+                using (var listener = new FileSystemWatcherEventListener(watcher))
+                {
+                    using (IFileStream stream = fileSystem.File.Open(pathToFileToWrite, FileMode.Open, FileAccess.ReadWrite))
+                    {
+                        // Act
+                        Task task = stream.WriteAsync(buffer, 0, buffer.Length);
+                        task.Wait();
+                    }
+
+                    watcher.FinishAndWaitForFlushed(NotifyWaitTimeoutMilliseconds);
+
+                    // Assert
+                    string text = string.Join(Environment.NewLine, listener.GetEventsCollectedAsText());
+                    text.Should().Be(expectedText);
+                }
+            }
+        }
+
+        [Theory]
+        [WatcherNotifyTestData(@"
+            * Container\file.txt                            @ LastAccess    LastWrite   Size
+        ")]
+        private void When_async_appending_to_stream_it_must_raise_events(NotifyFilters filters, [NotNull] string expectedText)
+        {
+            // Arrange
+            const string directoryToWatch = @"c:\some";
+            const string containerDirectoryName = "Container";
+            const string fileNameToWrite = "file.txt";
+
+            string pathToFileToWrite = Path.Combine(directoryToWatch, containerDirectoryName, fileNameToWrite);
+
+            FakeFileSystem fileSystem = new FakeFileSystemBuilder()
+                .IncludingBinaryFile(pathToFileToWrite, BufferFactory.Create(1024))
+                .Build();
+
+            var buffer = new byte[512];
+
+            using (FakeFileSystemWatcher watcher = fileSystem.ConstructFileSystemWatcher(directoryToWatch))
+            {
+                watcher.NotifyFilter = filters;
+                watcher.IncludeSubdirectories = true;
+
+                using (var listener = new FileSystemWatcherEventListener(watcher))
+                {
+                    using (IFileStream stream = fileSystem.File.Open(pathToFileToWrite, FileMode.Open, FileAccess.ReadWrite))
+                    {
+                        stream.Seek(0, SeekOrigin.End);
+
+                        // Act
+                        Task task = stream.WriteAsync(buffer, 0, buffer.Length);
+                        task.Wait();
+                    }
+
+                    watcher.FinishAndWaitForFlushed(NotifyWaitTimeoutMilliseconds);
+
+                    // Assert
+                    string text = string.Join(Environment.NewLine, listener.GetEventsCollectedAsText());
+                    text.Should().Be(expectedText);
                 }
             }
         }
