295e30c3b957810644b345f69f8c0e9dc28bf377
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2019-04-04 02:55:01 +0200

    removed workaround for framework differences in Path.IsPathRooted

---
+++ src/Fakes/PathCombiner.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace TestableFileSystem.Fakes
+{
+    internal static class PathCombiner
+    {
+        [NotNull]
+        public static string Combine([NotNull] string path1, [NotNull] string path2)
+        {
+            if (path2.Length == 0)
+            {
+                return path1;
+            }
+
+            if (path1.Length == 0 || IsPathRooted(path2))
+            {
+                return path2;
+            }
+
+            char lastChar1 = path1[path1.Length - 1];
+            if (!PathFacts.DirectorySeparatorChars.Contains(lastChar1) && lastChar1 != Path.VolumeSeparatorChar)
+            {
+                return path1 + Path.DirectorySeparatorChar + path2;
+            }
+
+            return path1 + path2;
+        }
+
+        private static bool IsPathRooted([NotNull] string path)
+        {
+            return StartsWithPathSeparator(path) || StartsWithDriveLetter(path);
+        }
+
+        private static bool StartsWithPathSeparator([NotNull] string path)
+        {
+            return path.Length >= 1 && PathFacts.DirectorySeparatorChars.Contains(path[0]);
+        }
+
+        private static bool StartsWithDriveLetter([NotNull] string path)
+        {
+            return path.Length >= 2 && path[1] == Path.VolumeSeparatorChar;
+        }
+    }
+}
--- src/Fakes/RelativePathConverter.cs
+++ src/Fakes/RelativePathConverter.cs
@@ -30,27 +30,8 @@ namespace TestableFileSystem.Fakes
 
             string currentDirectoryPath = currentDirectoryManager.GetValue().GetText();
 
-            string rooted = CombinePaths(currentDirectoryPath, path);
+            string rooted = PathCombiner.Combine(currentDirectoryPath, path);
             return new AbsolutePath(rooted);
-        }
-
-        [NotNull]
-        private static string CombinePaths([NotNull] string path1, [NotNull] string path2)
-        {
-            // TODO: We should not be depending on System.IO.Path.Combine() but have our own implementation to prevent issue below.
-
-            // NETCORE20 contains a bugfix for incorrect detection of rooted paths. The fix causes a different exception
-            // to be thrown at a later time when path2 contains "_:". To preserve our existing behavior, we check for
-            // the fixed scenario here.
-
-            if (path2.Length >= 2 && path2[1] == Path.VolumeSeparatorChar && !AbsolutePath.IsDriveLetter(path2.Substring(0, 2)))
-            {
-                // NETCORE20 combines both paths because it considers path2 non-rooted. That may be more correct, however
-                // it is not the behavior we used to get (and came to rely on).
-                return path2;
-            }
-
-            return Path.Combine(path1, path2);
         }
 
         [AssertionMethod]
